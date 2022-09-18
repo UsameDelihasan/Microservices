014e1ae138f6680fc291fbf2890ad366d6019f20
Üsame Delihasan <usamedelihasan14@gmail.com>
2022-09-18 04:49:19 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;   
+        }
+
+        [HttpGet("{Id}")]   
+        public ContactDTO GetContact(int id)
+        {
+            return _contactService.GetContactById(id);
+        }
+
+    }
+}
